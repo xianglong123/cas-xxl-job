390d0b3165e12731197fb2d5c953da13179ff208
xianglonggy <1391086179@qq.com>
2023-08-16 11:37:56 +0800

    init

---
+++ src/main/java/com/cas/Main.java
@@ -0,0 +1,12 @@
+package com.cas;
+
+/**
+ * @description: ${description}
+ * @author: xianglong
+ * @create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
